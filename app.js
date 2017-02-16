76fe9c25af16640810be77d2ac05d96d8765fa91
pmendoza72 <pablo_mendoza@hotmail.com>
2017-02-16 00:19:37 -0800

    got the total to work in js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ function Store(name, min, max, avgCookiesPerCustomer){
   this.name= name;
   this.min = min;
   this.max = max;
+  this.total = 0;
   this.avgCookiesPerCustomer = avgCookiesPerCustomer;
   this.customersPerHour = function(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -16,11 +17,13 @@ function Store(name, min, max, avgCookiesPerCustomer){
   this.generateCookies = function() {
     for (var i = 0; i < 8; i++){
       this.cookiesPerHour[i] = Math.round(this.customersPerHour(this.min, this.max)*this.avgCookiesPerCustomer);
+      this.total += this.cookiesPerHour[i];
     }
   }
 
   // Render a table row
   // for each cookie stand
+
 
   this.renderAsRow = function() {
     var cookieTable = document.getElementById('cookieTable');
@@ -30,17 +33,15 @@ function Store(name, min, max, avgCookiesPerCustomer){
     nameData.textContent = this.name;
     trElement.appendChild(nameData);
     cookieTable.appendChild(trElement);
+
     for (i = 0; i < 8; i++) {
       nameData = document.createElement('td');
       nameData.textContent = this.cookiesPerHour[i];
       trElement.appendChild(nameData);
-      cookieTable.appendChild(trElement);
     }
-    total = document.createElement('td');
-    total.textContent += this.cookiesPerHour[i];
-//    total += this.cookiesPerHour[i];
+    nameData = document.createElement('td');
+    nameData.textContent = this.total;
     trElement.appendChild(nameData);
-    cookieTable.appendChild(trElement);
   }
 }
 
